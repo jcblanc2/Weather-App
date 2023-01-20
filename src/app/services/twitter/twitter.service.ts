47217a738502cb0ab3d62d9c1b0934f1b1c8d2ae
jcblanc2 <jclaytonblanc@gmail.com>
2023-01-19 19:08:42 -0500

    add comments

--- src/app/services/twitter/twitter.service.ts
+++ src/app/services/twitter/twitter.service.ts
@@ -6,34 +6,4 @@ import {filter, first, map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class TwitterService {
-  api_url = 'https://localhost:3000';
-
-  constructor(public http: HttpClient) {
-  }
-  fetchTweets(city:string) {
-    return this.http.post('https://us-central1-minimus-weather.cloudfunctions.net/tweets', {
-      data: {q: `${city} Weather`}
-    }).pipe(
-      first(),
-      map((res: any) => res && res.result ? res.result.statuses : []),
-      filter((tweets: any) => tweets.map((tweet:any) => tweet.text && tweet.text.match(/weather/g))),
-      map((tweets: any) => tweets.map((tweet:any) => ({
-        text: tweet.text,
-        date: tweet.created_at,
-        user: {
-          name: tweet.user.name,
-          photo: tweet.user.profile_image_url_https,
-          handle: tweet.user.screen_name
-        }
-      }))),
-      map((tweets: any) => tweets.slice(0, 4)));
-  }
-
-
-  getTimeline() {
-    return this.http
-      .get<any[]>(this.api_url+'/home_timeline')
-      .pipe(map(data => data));
- 
-  }
 }
